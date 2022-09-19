2c465c09a440e5d4339c74dad2684069befbe925
= <37115588+Maya084@users.noreply.github.com>
2022-09-19 12:57:38 +0200

    🎉 Init: initial commit
    display time (hours, minutes, seconds) as hex value and change the background color to that value

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  $: str = "#123456";
+
+  function convertNum(time: number): string {
+    return ("0" + time).slice(-2);
+  }
+
+  function doDate() {
+    let time = new Date();
+    const helper =
+      "#" +
+      convertNum(time.getHours()) + 
+      convertNum(time.getMinutes()) + 
+      convertNum(time.getSeconds());
+
+    str = helper;
+    document.documentElement.style.setProperty("--background-color", str);
+  }
+  onMount(() => {
+    setInterval(doDate, 1000);
+  });
+</script>
+
+<main class="main-content">
+  <div class="time-value">{str}</div>
+</main>
+
+<style>
+
+  .time-value {
+    font-family: "Roboto Mono";
+    color: #e3ede5;
+    font-size: 13vw;
+    font-weight: 700;
+  }
+</style>
